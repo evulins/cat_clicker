ad62ed62e725a429086e3320354f4d94e30e9e6a
Ewelina Sara-Cichuta <evelinasarad@gmail.com>
2018-05-24 17:31:56 +0200

    two cats

--- js/app.js
+++ js/app.js
@@ -1,13 +1,52 @@
 // Global variables
 
-let clickCounter = 0;
 let gameStarted = false;
 
+let cats = {
+    list: [ 
+        {name: 'Stefka', image: 'stefka.jpg'},
+        {name: 'Lola', image: 'lola.jpg'},
+        {name: 'Mokra Stefka', image: 'mokraStefka.jpg'},
+        {name: 'Mokra Lola', image: 'mokraLola.jpg'},
+        {name: 'Dziewczyny', image: 'cats.jpg'}
+    ],
 
-// Increments and updates the move counter
-function updateClickCounter() {
-    clickCounter += 1;
-    $('span.clicks').text(clickCounter);
+    getAll: function() {
+            return cats.list;
+    }
+};
+
+function showCats(catsList) {
+
+    for (let i = 0; i < catsList.length; i++) {
+        const current = catsList[i];
+        const cat = $(`
+            <li class="cat">
+                <div class="catInfo">
+                    <p class="name"><span>${current.name}</span></p>
+                    <span class="clicks">0</span> clicks
+                </div>
+                <div class="image">
+                    <img src="images/${current.image}">
+                </div>
+            </li>
+        `);
+        const counter = cat.find('span.clicks');
+
+        cat.find('.image').on('click', (function(counter) {
+            return function() {
+                const clicks = counter.text();
+                counter.text(parseInt(clicks) + 1);
+                if (gameStarted === false) {
+                    gameStarted = true;
+                    $('.runner').runner('start');
+                }
+            };
+        })(counter));
+
+        // Adds all cats to list .catsList
+        $('.catsList').append(cat);
+    }  
 }
 
 // Resets the game and the score
@@ -23,18 +62,20 @@ function startGame() {
     //Runs the stopwatch
     $('.runner').runner();
 
-//Adds the event listener for a clicked card
-    $('.cats').on('click', function(event) {
-        event.preventDefault();
-        updateClickCounter();
+//Adds the event listener for a clicked cat
+    // $('img').on('click', function(event) {
+    //     event.preventDefault();
+    //     updateClickCounter();
 
-        if (gameStarted === false) {
-            gameStarted = true;
-            $('.runner').runner('start');
-        }
-    });
+    //     if (gameStarted === false) {
+    //         gameStarted = true;
+    //         $('.runner').runner('start');
+    //     }
+    // });
 }
 
 (function() {
+    showCats(cats.getAll());
     startGame();
 })();
+
